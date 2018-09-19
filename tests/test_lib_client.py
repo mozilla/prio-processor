c2df4d7c87f182e837053d402b349ccdc876221d
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-09-19 18:05:33 -0400

    Add tests for `libprio.PublicKey_export`

--- tests/test_lib_client.py
+++ tests/test_lib_client.py
@@ -63,3 +63,38 @@ def test_publickey_export():
     raw_bytes2 = prio.PublicKey_export(pubkey)
 
     assert raw_bytes == raw_bytes2
+
+
+@pytest.mark.parametrize("hex_bytes", [
+    b"102030405060708090A0B0C0D0E0F00000FFEEDDCCBBAA998877665544332211",
+    b"102030405060708090a0B0C0D0E0F00000FfeEddcCbBaa998877665544332211"
+])
+def test_publickey_import_hex(hex_bytes):
+    expect = bytes([
+        0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0, 0xB0,
+        0xC0, 0xD0, 0xE0, 0xF0, 0x00, 0x00, 0xFF, 0xEE, 0xDD, 0xCC, 0xBB,
+        0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11
+    ])
+    pubkey = prio.PublicKey_import_hex(hex_bytes)
+    raw_bytes = prio.PublicKey_export(pubkey)
+
+    assert raw_bytes == expect
+
+
+def test_publickey_import_hex_bad_length_raises_exception():
+    hex_bytes = b"102030405060708090A"
+    with pytest.raises(RuntimeError):
+        prio.PublicKey_import_hex(hex_bytes)
+
+
+def test_publickey_export_hex():
+    # the output includes the null-byte
+    expect = b"102030405060708090A0B0C0D0E0F00000FFEEDDCCBBAA998877665544332211\0"
+    raw_bytes = bytes([
+        0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xA0, 0xB0,
+        0xC0, 0xD0, 0xE0, 0xF0, 0x00, 0x00, 0xFF, 0xEE, 0xDD, 0xCC, 0xBB,
+        0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11
+    ])
+    pubkey = prio.PublicKey_import(raw_bytes)
+    hex_bytes = prio.PublicKey_export_hex(pubkey)
+    assert bytes(hex_bytes) == expect
