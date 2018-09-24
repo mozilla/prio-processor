5f3e8fc2c948a5e78e1f3ee548262d459f05e0e6
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-09-23 20:56:03 -0700

    Add failing tests for verifier serialization

---
+++ tests/test_serialize.py
@@ -0,0 +1,69 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pickle
+import pytest
+from prio import prio
+
+
+@pytest.fixture
+def seed():
+    return prio.PRGSeed()
+
+@pytest.fixture
+def serverA_keypair():
+    return prio.create_keypair()
+
+@pytest.fixture
+def serverB_keypair():
+    return prio.create_keypair()
+
+@pytest.fixture
+def config(serverA_keypair, serverB_keypair):
+    _, pkA = serverA_keypair
+    _, pkB = serverB_keypair
+    return prio.Config(133, pkA, pkB, b"test_batch")
+
+@pytest.fixture
+def serverA(seed, config, serverA_keypair):
+    sk, _ = serverA_keypair
+    return prio.Server(config, prio.PRIO_SERVER_A, sk, seed)
+
+@pytest.fixture
+def serverB(seed, config, serverB_keypair):
+    sk, _ = serverB_keypair
+    return prio.Server(config, prio.PRIO_SERVER_B, sk, seed)
+
+@pytest.fixture
+def client(config):
+    return prio.Client(config)
+
+
+def test_serialize_verifier(config, client, serverA, serverB):
+    n_data = config.num_data_fields()
+    data_items = bytearray([(i % 3 == 1) or (i % 5 == 1) for i in range(n_data)])
+
+    for_server_a, for_server_b = client.encode(data_items)
+
+    def is_valid(vA):
+        vB = serverB.create_verifier(for_server_b)
+
+        p1A = vA.create_verify1()
+        p1B = vB.create_verify1()
+
+        p2A = vA.create_verify2(p1A, p1B)
+        p2B = vB.create_verify2(p1A, p1B)
+
+        return vA.is_valid(p2A, p2B) and vB.is_valid(p2A, p2B)
+
+    vA = serverA.create_verifier(for_server_a)
+    assert is_valid(vA)
+
+    vA_ser = pickle.loads(pickle.dumps(vA))
+    assert vA is not vA_ser
+
+    # free the pointer instance and double check that the verifier is
+    # actually being serialized
+    del vA
+    assert is_valid(vA_ser)
