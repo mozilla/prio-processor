5b820d17a119a99e0c4a07edc71271fd99cc3c1e
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-09-23 21:14:39 -0700

    Add failing tests for verify1, verify2, and total shares

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -67,3 +67,85 @@ def test_serialize_verifier(config, client, serverA, serverB):
     # actually being serialized
     del vA
     assert is_valid(vA_ser)
+
+
+def test_serialize_verify1(config, client, serverA, serverB):
+    n_data = config.num_data_fields()
+    data_items = bytearray([(i % 3 == 1) or (i % 5 == 1) for i in range(n_data)])
+
+    for_server_a, for_server_b = client.encode(data_items)
+
+    vA = serverA.create_verifier(for_server_a)
+    vB = serverB.create_verifier(for_server_b)
+
+    def is_valid(p1A):
+        p1B = vB.create_verify1()
+
+        p2A = vA.create_verify2(p1A, p1B)
+        p2B = vB.create_verify2(p1A, p1B)
+
+        return vA.is_valid(p2A, p2B) and vB.is_valid(p2A, p2B)
+
+    p1A = vA.create_verify1()
+    assert is_valid(p1A)
+
+    p1A_ser = pickle.loads(pickle.dumps(p1A))
+    del p1A
+    assert is_valid(p1A_ser)
+
+
+def test_serialize_verify2(config, client, serverA, serverB):
+    n_data = config.num_data_fields()
+    data_items = bytearray([(i % 3 == 1) or (i % 5 == 1) for i in range(n_data)])
+
+    for_server_a, for_server_b = client.encode(data_items)
+
+    vA = serverA.create_verifier(for_server_a)
+    vB = serverB.create_verifier(for_server_b)
+
+    p1A = vA.create_verify1()
+    p1B = vB.create_verify1()
+
+    def is_valid(p2A):
+        p2B = vB.create_verify2(p1A, p1B)
+        return vA.is_valid(p2A, p2B) and vB.is_valid(p2A, p2B)
+
+    p2A = vA.create_verify2(p1A, p1B)
+    assert is_valid(p2A)
+
+    p2A_ser = pickle.loads(pickle.dumps(p2A))
+    del p2A
+    assert is_valid(p2A_ser)
+
+
+def test_serialize_total_shares(config, client, serverA, serverB):
+    n_data = config.num_data_fields()
+    data_items = bytearray([(i % 3 == 1) or (i % 5 == 1) for i in range(n_data)])
+
+    for_server_a, for_server_b = client.encode(data_items)
+
+    vA = serverA.create_verifier(for_server_a)
+    vB = serverB.create_verifier(for_server_b)
+
+    p1A = vA.create_verify1()
+    p1B = vB.create_verify1()
+
+    p2A = vA.create_verify2(p1A, p1B)
+    p2B = vB.create_verify2(p1A, p1B)
+
+    assert vA.is_valid(p2A, p2B) and vB.is_valid(p2A, p2B)
+
+    serverA.aggregate(vA)
+    serverB.aggregate(vB)
+
+    def is_expected(t_a):
+        t_b = serverB.total_shares()
+        output = prio.total_share_final(config, t_a, t_b)
+        return list(output) == list(data_items)
+
+    t_a = serverA.total_shares()
+    assert is_expected(t_a)
+
+    t_a_ser = pickle.loads(pickle.dumps(t_a))
+    del t_a
+    assert is_expected(t_a_ser)
