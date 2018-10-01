56cbe2ed165255ed4895434f27a36f73d538640c
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2018-09-30 18:14:21 -0700

    Add setup for exchanging public keys (synchronously)

--- examples/docker-asyncio/server.py
+++ examples/docker-asyncio/server.py
@@ -1,25 +1,29 @@
 import asyncio
 import aioamqp
 import logging
+import os
+
+from prio import prio
+import rpc
 
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
-async def on_message(channel, body, envelope, properties):
-    logger.info(body)
-
+server_id = int(os.environ['SERVER_ID'])
+assert server_id in (prio.PRIO_SERVER_A, prio.PRIO_SERVER_B)
+other_server_id = 0 if server_id == 1 else 1
+private_key, public_key = prio.create_keypair()
 
 async def main():
     queue_name = "test"
     logger.info("Starting server")
 
     transport, protocol = await aioamqp.connect("rabbitmq", 5672, "guest", "guest")
-    channel = await protocol.channel()
-    await channel.queue_declare(queue_name=queue_name)
-    await channel.basic_consume(on_message, queue_name=queue_name, no_ack=True)
-
+    await rpc.Server(protocol, lambda: public_key.export_hex(), server_id).connect()
+    await asyncio.sleep(1)
+    other_key = await rpc.Client(protocol).call(other_server_id)
+    logger.info(other_key)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
