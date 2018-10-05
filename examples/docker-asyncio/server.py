06f47cbfc23c45ffc36207fdf514df23573063bc
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-10-04 17:54:29 -0700

    Use aio-pika instead of aioamqp

--- examples/docker-asyncio/server.py
+++ examples/docker-asyncio/server.py
@@ -1,5 +1,5 @@
 import asyncio
-import aioamqp
+import aio_pika
 import logging
 import os
 import sys
@@ -15,90 +15,12 @@ logger.setLevel(logging.INFO)
 
 DEFAULT_SHARED_SEED=b'vY\xc1\t\x93\xfb\xc6\x97*\x07j\xd63i+\x86'
 
-
 def get_other_server(server_id):
     mapping = {
         prio.PRIO_SERVER_A: prio.PRIO_SERVER_B,
         prio.PRIO_SERVER_B: prio.PRIO_SERVER_A
     }
     return mapping[server_id]
-
-
-class PrioHandler:
-    def __init__(self, protocol, server):
-        # A cache when waiting for the other server's packets. In the case where the
-        # message handler is threaded, the built-in python types are inherently thread-safe.
-        # However, asyncio runs on a single thread by default.
-        self.cache = {}
-
-        # prio data structures
-        self.server = server
-
-        # amqp setup
-        self.protocol = protocol
-        self.channel = None
-
-    async def connect(self):
-        self.channel = await self.protocol.channel()
-        queue_name = "prio.{}".format(self.server.server_id)
-        await self.channel.queue_declare(queue_name=queue_name)
-        await self.channel.basic_consume(self.on_message, queue_name=queue_name)
-
-    async def on_message(self, channel, body, envelope, properties):
-        pid = properties.message_id
-        v, p1, p2 = self.cache.get(pid, (None, None, None))
-
-        def log(line):
-            logger.info("Message {}: {}".format(pid, line))
-
-        ptype = properties.type
-        routing_key = "prio.{}".format(get_other_server(self.server.server_id))
-
-        if (ptype == 'verify1' and not p1) or (ptype == 'verify2' and not p2):
-            log("Re-queuing message!")
-            await self.channel.basic_publish(
-                payload=body,
-                exchange=envelope.exchange,
-                routing_key=envelope.routing_key,
-                properties=properties
-            )
-        elif ptype == 'data':
-            log("Generating verify packet 1")
-            v = self.server.create_verifier(bytes(body))
-            p1 = v.create_verify1()
-            await self.channel.basic_publish(
-                payload=pickle.dumps(p1),
-                exchange=envelope.exchange,
-                routing_key=routing_key,
-                properties={
-                    'message_id': properties.message_id,
-                    'type': 'verify1'
-                }
-            )
-        elif ptype == 'verify1':
-            log("Generating verify packet 2")
-            p2 = v.create_verify2(p1, pickle.loads(body))
-            await self.channel.basic_publish(
-                payload=pickle.dumps(p2),
-                exchange=envelope.exchange,
-                routing_key=routing_key,
-                properties={
-                    'message_id': properties.message_id,
-                    'type': 'verify2'
-                }
-            )
-        elif ptype == 'verify2':
-            if v.is_valid(p2, pickle.loads(body)):
-                log("Aggregate data")
-                self.server.aggregate(v)
-            else:
-                log("Invalid data")
-            del self.cache[pid]
-        else:
-            log("Bad message type {}".format(ptype))
-
-        await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
-        self.cache[pid] = (v, p1, p2)
 
 
 async def run_server(server_id, n_fields, batch_id, shared_seed):
@@ -118,8 +40,67 @@ async def run_server(server_id, n_fields, batch_id, shared_seed):
     pvtkey = skA if server_id == prio.PRIO_SERVER_A else skB
     server = prio.Server(config, server_id, pvtkey, seed)
 
-    transport, protocol = await aioamqp.connect("rabbitmq", 5672, "guest", "guest")
-    await PrioHandler(protocol, server).connect()
+    cache = {}
+
+    connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
+    channel = await connection.channel()
+    queue = await channel.declare_queue("prio.{}".format(server_id))
+
+    async for message in queue:
+        with message.process():
+            pid = message.message_id
+            v, p1, p2 = cache.get(pid, (None, None, None))
+
+            def log(line):
+                logger.info("Message {}: {}".format(pid, line))
+
+            ptype = message.type
+            routing_key = "prio.{}".format(get_other_server(server_id))
+
+            if (ptype == 'verify1' and not p1) or (ptype == 'verify2' and not p2):
+                log("Re-queuing message!")
+                await channel.default_exchange.publish(
+                    aio_pika.Message(
+                        body=message.body,
+                        message_id=message.message_id,
+                        type=message.type
+                    ),
+                    routing_key="prio.{}".format(server_id)
+                )
+            elif ptype == 'data':
+                log("Generating verify packet 1")
+                v = server.create_verifier(message.body)
+                p1 = v.create_verify1()
+                await channel.default_exchange.publish(
+                    aio_pika.Message(
+                        body=pickle.dumps(p1),
+                        message_id=message.message_id,
+                        type='verify1'
+                    ),
+                    routing_key=routing_key
+                )
+            elif ptype == 'verify1':
+                log("Generating verify packet 2")
+                p2 = v.create_verify2(p1, pickle.loads(message.body))
+                await channel.default_exchange.publish(
+                    aio_pika.Message(
+                        body=pickle.dumps(p2),
+                        message_id=message.message_id,
+                        type='verify2'
+                    ),
+                    routing_key=routing_key
+                )
+            elif ptype == 'verify2':
+                if v.is_valid(p2, pickle.loads(message.body)):
+                    log("Aggregate data")
+                    server.aggregate(v)
+                else:
+                    log("Invalid data")
+                del cache[pid]
+            else:
+                log("Bad message type {}".format(ptype))
+
+            cache[pid] = (v, p1, p2)
 
 
 @click.command()
