89d578bf6a84096d220b7a0aee475dbb70999c42
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-10-04 17:54:29 -0700

    Use aio-pika instead of aioamqp

--- examples/docker-asyncio/client.py
+++ examples/docker-asyncio/client.py
@@ -1,10 +1,9 @@
 import asyncio
-import aioamqp
+import aio_pika
 import logging
 import click
 
 from prio import prio
-import rpc
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -12,8 +11,6 @@ logger.setLevel(logging.INFO)
 
 
 async def run_client(n_clients, n_fields, batch_id):
-    transport, protocol = await aioamqp.connect("rabbitmq", 5672, "guest", "guest")
-
     await asyncio.sleep(3)
     pkey_a = b'F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51'
     pkey_b = b'15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07'
@@ -25,46 +22,33 @@ async def run_client(n_clients, n_fields, batch_id):
 
     data_items = bytes([(i % 3 == 1) or (i % 5 == 1) for i in range(n_fields)])
 
-    seed = prio.PRGSeed()
-    seed.instance = b'vY\xc1\t\x93\xfb\xc6\x97*\x07j\xd63i+\x86'
-    skey_a = b'7A0AA608C08CB74A86409F5026865435B2F17F40B20636CEFD2656585097FBE0'
-    skey_b = b'50C7329DE18DE3087A0DE963D5585A4DB7A156C7A29FA854760373B053D86919'
-    skA = prio.PrivateKey().import_hex(skey_a, pkey_a)
-    skB = prio.PrivateKey().import_hex(skey_b, pkey_b)
-    server_a = prio.Server(config, prio.PRIO_SERVER_A, skA, seed)
-    server_b = prio.Server(config, prio.PRIO_SERVER_B, skB, seed)
+    connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
+    channel = await connection.channel()
+    await channel.declare_queue("prio.0")
+    await channel.declare_queue("prio.1")
 
-    channel = await protocol.channel()
-    # send messages along the main prio queues
-    await channel.queue_declare(queue_name="prio.0")
-    await channel.queue_declare(queue_name="prio.1")
     for i in range(n_clients):
+
         logger.info("Client {}: Generated shares".format(i))
         for_server_a, for_server_b = client.encode(data_items)
-        logger.info("A: {} B: {}".format(len(for_server_a), len(for_server_b)))
-        server_a.create_verifier(for_server_a).create_verify1()
-        server_b.create_verifier(for_server_b).create_verify1()
-        await channel.basic_publish(
-            payload=for_server_a,
-            exchange_name='',
-            routing_key="prio.0",
-            properties={
-                'message_id': str(i),
-                'type': 'data'
-            }
-        )
-        await channel.basic_publish(
-            payload=for_server_b,
-            exchange_name='',
-            routing_key="prio.1",
-            properties={
-                'message_id': str(i),
-                'type': 'data'
-            }
-        )
 
-    await protocol.close()
-    transport.close()
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body=for_server_a,
+                message_id=str(i),
+                type='data'
+            ),
+            routing_key="prio.0"
+        )
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body=for_server_b,
+                message_id=str(i),
+                type='data'
+            ),
+            routing_key="prio.1"
+        )
+    await connection.close()
     logger.info("Client done!")
 
 
