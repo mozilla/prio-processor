7c225c408f8bf578a2dbc581768da86fed3498f6
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-10-04 17:22:00 -0700

    Use a fixed pub/pvt key for testing

--- examples/docker-asyncio/client.py
+++ examples/docker-asyncio/client.py
@@ -14,20 +14,25 @@ logger.setLevel(logging.INFO)
 async def run_client(n_clients, n_fields, batch_id):
     transport, protocol = await aioamqp.connect("rabbitmq", 5672, "guest", "guest")
 
-    await asyncio.sleep(5)
-    logging.info("Collecting public keys")
-    server_a_data = await rpc.Client(protocol).call(prio.PRIO_SERVER_A)
-    server_b_data = await rpc.Client(protocol).call(prio.PRIO_SERVER_B)
-    logging.info(
-        "Public keys:\n Server A: {}\nServer B: {}"
-        .format(server_a_data, server_b_data))
-    server_a_pubkey = prio.PublicKey().import_hex(server_a_data)
-    server_b_pubkey = prio.PublicKey().import_hex(server_b_data)
+    await asyncio.sleep(3)
+    pkey_a = b'F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51'
+    pkey_b = b'15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07'
+    pkA = prio.PublicKey().import_hex(pkey_a)
+    pkB = prio.PublicKey().import_hex(pkey_b)
 
-    config = prio.Config(n_fields, server_a_pubkey, server_b_pubkey, batch_id)
+    config = prio.Config(n_fields, pkA, pkB, batch_id)
     client = prio.Client(config)
 
     data_items = bytes([(i % 3 == 1) or (i % 5 == 1) for i in range(n_fields)])
+
+    seed = prio.PRGSeed()
+    seed.instance = b'vY\xc1\t\x93\xfb\xc6\x97*\x07j\xd63i+\x86'
+    skey_a = b'7A0AA608C08CB74A86409F5026865435B2F17F40B20636CEFD2656585097FBE0'
+    skey_b = b'50C7329DE18DE3087A0DE963D5585A4DB7A156C7A29FA854760373B053D86919'
+    skA = prio.PrivateKey().import_hex(skey_a, pkey_a)
+    skB = prio.PrivateKey().import_hex(skey_b, pkey_b)
+    server_a = prio.Server(config, prio.PRIO_SERVER_A, skA, seed)
+    server_b = prio.Server(config, prio.PRIO_SERVER_B, skB, seed)
 
     channel = await protocol.channel()
     # send messages along the main prio queues
@@ -37,6 +42,8 @@ async def run_client(n_clients, n_fields, batch_id):
         logger.info("Client {}: Generated shares".format(i))
         for_server_a, for_server_b = client.encode(data_items)
         logger.info("A: {} B: {}".format(len(for_server_a), len(for_server_b)))
+        server_a.create_verifier(for_server_a).create_verify1()
+        server_b.create_verifier(for_server_b).create_verify1()
         await channel.basic_publish(
             payload=for_server_a,
             exchange_name='',
