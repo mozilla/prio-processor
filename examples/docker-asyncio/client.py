d704661b9d4273d1ca16cdc6d7e02aadf6902d8f
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2018-11-13 14:05:48 -0800

    Reduce docker-example to message exchanges

--- examples/docker-asyncio/client.py
+++ examples/docker-asyncio/client.py
@@ -11,17 +11,16 @@ logger.setLevel(logging.INFO)
 
 
 async def run_client(n_clients, n_fields, batch_id):
-    await asyncio.sleep(3)
-    # pkey_a = b'F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51'
-    # pkey_b = b'15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07'
-
     connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
     channel = await connection.channel()
-    rpc = aio_pika.patterns.RPC.create(channel)
-
     await channel.declare_queue("prio.0")
     await channel.declare_queue("prio.1")
 
+    # delay for server setup
+    await asyncio.sleep(3)
+
+    pkey_a = b"F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51"
+    pkey_b = b"15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07"
     pkA = prio.PublicKey().import_hex(pkey_a)
     pkB = prio.PublicKey().import_hex(pkey_b)
 
@@ -36,33 +35,26 @@ async def run_client(n_clients, n_fields, batch_id):
         for_server_a, for_server_b = client.encode(data_items)
 
         await channel.default_exchange.publish(
-            aio_pika.Message(
-                body=for_server_a,
-                message_id=str(i),
-                type='data'
-            ),
-            routing_key="prio.0"
+            aio_pika.Message(body=for_server_a, message_id=str(i), type="data"),
+            routing_key="prio.0",
         )
         await channel.default_exchange.publish(
-            aio_pika.Message(
-                body=for_server_b,
-                message_id=str(i),
-                type='data'
-            ),
-            routing_key="prio.1"
+            aio_pika.Message(body=for_server_b, message_id=str(i), type="data"),
+            routing_key="prio.1",
         )
     await connection.close()
     logger.info("Client done!")
 
 
 @click.command()
-@click.option('--n-clients', type=int, default=10)
-@click.option('--n-fields', type=int, required=True)
-@click.option('--batch-id', type=str, default='test_batch')
+@click.option("--n-clients", type=int, default=10)
+@click.option("--n-fields", type=int, required=True)
+@click.option("--batch-id", type=str, default="test_batch")
 def main(n_clients, n_fields, batch_id):
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run_client(n_clients, n_fields, bytes(batch_id, "utf-8")))
     loop.close()
+
 
 if __name__ == "__main__":
     main()
--- examples/docker-asyncio/server.py
+++ examples/docker-asyncio/server.py
@@ -8,41 +8,35 @@ from functools import partial
 
 import click
 from prio import prio
-# import rpc
 
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-DEFAULT_SHARED_SEED=b'vY\xc1\t\x93\xfb\xc6\x97*\x07j\xd63i+\x86'
+DEFAULT_SHARED_SEED = b"vY\xc1\t\x93\xfb\xc6\x97*\x07j\xd63i+\x86"
+
 
 def get_other_server(server_id):
     mapping = {
         prio.PRIO_SERVER_A: prio.PRIO_SERVER_B,
-        prio.PRIO_SERVER_B: prio.PRIO_SERVER_A
+        prio.PRIO_SERVER_B: prio.PRIO_SERVER_A,
     }
     return mapping[server_id]
 
 
 async def run_server(server_id, n_fields, batch_id, shared_seed):
-    # skey_a = b'7A0AA608C08CB74A86409F5026865435B2F17F40B20636CEFD2656585097FBE0'
-    # pkey_a = b'F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51'
-    # skey_b = b'50C7329DE18DE3087A0DE963D5585A4DB7A156C7A29FA854760373B053D86919'
-    # pkey_b = b'15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07'
-    # pkA = prio.PublicKey().import_hex(pkey_a)
-    # pkB = prio.PublicKey().import_hex(pkey_b)
-    # skA = prio.PrivateKey().import_hex(skey_a, pkey_a)
-    # skB = prio.PrivateKey().import_hex(skey_b, pkey_b)
-
     connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
     channel = await connection.channel()
     queue = await channel.declare_queue(f"prio.{server_id}")
 
-    rpc = aio_pika.patterns.RPC.create(channel)
-
-    pkA, skA = prio.create_keypair()
-    await rpc.register("publickey", lambda: pkA.export_hex(), {"name": f"prio.rpc.{server_id}"})
-    pkB = await rpc.proxy.publickey(get_other_server(server_id))
+    skey_a = b"7A0AA608C08CB74A86409F5026865435B2F17F40B20636CEFD2656585097FBE0"
+    pkey_a = b"F63F2FB9B823B7B672684A526AC467DCFC110D4BB242F6DF0D3EA9F09CE14B51"
+    skey_b = b"50C7329DE18DE3087A0DE963D5585A4DB7A156C7A29FA854760373B053D86919"
+    pkey_b = b"15DC84D87C73A36120E0389D4ABCD433EDC5147DC71A4093E2A5952968D51F07"
+    pkA = prio.PublicKey().import_hex(pkey_a)
+    pkB = prio.PublicKey().import_hex(pkey_b)
+    skA = prio.PrivateKey().import_hex(skey_a, pkey_a)
+    skB = prio.PrivateKey().import_hex(skey_b, pkey_b)
 
     seed = prio.PRGSeed()
     seed.instance = shared_seed
@@ -64,17 +58,17 @@ async def run_server(server_id, n_fields, batch_id, shared_seed):
             ptype = message.type
             routing_key = "prio.{}".format(get_other_server(server_id))
 
-            if (ptype == 'verify1' and not p1) or (ptype == 'verify2' and not p2):
+            if (ptype == "verify1" and not p1) or (ptype == "verify2" and not p2):
                 log("Re-queuing message!")
                 await channel.default_exchange.publish(
                     aio_pika.Message(
                         body=message.body,
                         message_id=message.message_id,
-                        type=message.type
+                        type=message.type,
                     ),
-                    routing_key="prio.{}".format(server_id)
+                    routing_key="prio.{}".format(server_id),
                 )
-            elif ptype == 'data':
+            elif ptype == "data":
                 log("Generating verify packet 1")
                 v = server.create_verifier(message.body)
                 p1 = v.create_verify1()
@@ -82,22 +76,22 @@ async def run_server(server_id, n_fields, batch_id, shared_seed):
                     aio_pika.Message(
                         body=pickle.dumps(p1),
                         message_id=message.message_id,
-                        type='verify1'
+                        type="verify1",
                     ),
-                    routing_key=routing_key
+                    routing_key=routing_key,
                 )
-            elif ptype == 'verify1':
+            elif ptype == "verify1":
                 log("Generating verify packet 2")
                 p2 = v.create_verify2(p1, pickle.loads(message.body))
                 await channel.default_exchange.publish(
                     aio_pika.Message(
                         body=pickle.dumps(p2),
                         message_id=message.message_id,
-                        type='verify2'
+                        type="verify2",
                     ),
-                    routing_key=routing_key
+                    routing_key=routing_key,
                 )
-            elif ptype == 'verify2':
+            elif ptype == "verify2":
                 if v.is_valid(p2, pickle.loads(message.body)):
                     log("Aggregate data")
                     server.aggregate(v)
@@ -111,14 +105,17 @@ async def run_server(server_id, n_fields, batch_id, shared_seed):
 
 
 @click.command()
-@click.option('--server-id', type=click.Choice(['a', 'b']), required=True)
-@click.option('--n-fields', type=int, required=True)
-@click.option('--batch-id', type=str, default='test_batch')
+@click.option("--server-id", type=click.Choice(["a", "b"]), required=True)
+@click.option("--n-fields", type=int, required=True)
+@click.option("--batch-id", type=str, default="test_batch")
 def main(server_id, n_fields, batch_id):
     loop = asyncio.get_event_loop()
-    server_id = prio.PRIO_SERVER_A if server_id == 'a' else prio.PRIO_SERVER_B
-    loop.run_until_complete(run_server(server_id, n_fields, bytes(batch_id, "utf-8"), DEFAULT_SHARED_SEED))
+    server_id = prio.PRIO_SERVER_A if server_id == "a" else prio.PRIO_SERVER_B
+    loop.run_until_complete(
+        run_server(server_id, n_fields, bytes(batch_id, "utf-8"), DEFAULT_SHARED_SEED)
+    )
     loop.run_forever()
+
 
 if __name__ == "__main__":
     main()
