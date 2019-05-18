4cecfae77dde345a9e1b27bbcd30a6747bb22b2c
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-05-17 21:35:30 -0700

    Create broadcast variables for server and config using serialiable partial functions

--- prio/spark/transform.py
+++ prio/spark/transform.py
@@ -19,9 +19,41 @@ def publish(df):
     pass
 
 
+from functools import partial
 import click
 from ..cli import options
-from ..cli.commands import match_server, import_keys
+from ..cli.commands import match_server, import_keys, import_public_keys
+
+
+def create_config(
+    public_key_hex_internal, public_key_hex_external, n_data, batch_id
+):
+    _, public_key_internal, public_key_external = import_public_keys(
+        private_key_hex, public_key_hex_internal, public_key_hex_external
+    )
+    return libprio.PrioConfig_new(
+        n_data, public_key_internal, public_key_external, batch_id
+    )
+
+
+def create_server(
+    private_key_hex,
+    public_key_hex_internal,
+    public_key_hex_external,
+    n_data,
+    batch_id,
+    server_id,
+    shared_secret,
+):
+    private_key, public_key_internal, public_key_external = import_keys(
+        private_key_hex, public_key_hex_internal, public_key_hex_external
+    )
+    config = libprio.PrioConfig_new(
+        n_data, public_key_internal, public_key_external, batch_id
+    )
+    return libprio.PrioServer_new(
+        config, match_server(server_id), private_key, b64decode(shared_secret)
+    )
 
 
 @click.command()
@@ -46,25 +78,29 @@ def prio_spark(
 ):
     spark = SparkSession.builder.appName("prio").getOrCreate()
 
-    def create_config():
-        private_key, public_key_internal, public_key_external = import_keys(
-            private_key_hex, public_key_hex_internal, public_key_hex_external
+    broadcast_config_func = spark.sparkContext.broadcast(
+        partial(
+            create_config,
+            public_key_hex_internal,
+            public_key_hex_external,
+            n_data,
+            batch_id,
         )
-        return libprio.PrioConfig_new(
-            n_data, public_key_internal, public_key_external, batch_id
+    )
+    broadcast_server_func = spark.sparkContext.broadcast(
+        partial(
+            create_server,
+            private_key_hex,
+            public_key_hex_internal,
+            public_key_hex_external,
+            n_data,
+            batch_id,
+            server_id,
+            shared_secret,
         )
-
-    def create_server():
-        config = create_config()
-        return libprio.PrioServer_new(
-            config, match_server(server_id), private_key, b64decode(shared_secret)
-        )
-
-    config_var = spark.sparkContext.broadcast(create_config)
-    server_var = spark.sparkContext.broadcast(create_server)
+    )
 
     import code
-
     code.interact(local=locals())
 
 
