fd4dc117ce778194511ad4ba85a9c27453549146
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-05-17 17:12:14 -0700

    Add a wait_for_data function

--- examples/batched-processing/scripts/main.sh
+++ examples/batched-processing/scripts/main.sh
@@ -18,11 +18,31 @@ set -x
 
 mc config host add minio http://minio:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api s3v4;
 
+function wait_for_data() {
+    path=$(( mc watch --json $1 & ) | head -n 1 | jq -r '.events.path')
+    # Remove the url portion
+    echo "minio/${path##http://minio:9000/}"
+}
+
+sleep 3
+mkdir -p data/raw
+mkdir -p data/intermediate/internal/verify1
+mkdir -p data/intermediate/internal/verify2
+mkdir -p data/intermediate/internal/aggregate
+mkdir -p data/intermediate/external/verify1
+mkdir -p data/intermediate/external/verify2
+mkdir -p data/intermediate/external/aggregate
+mkdir -p data/processed
+cd data
+
 ###########################################################
 # verify1
 ###########################################################
 
-path=$(( mc watch --json & ) | head -n 1 | jq -r '.events.path')
+path=$(wait_for_data "minio/$BUCKET_INTERNAL/raw/")
+filename=$(basename $path)
+
+mc cp $path raw/
 
 prio verify1 \
     --n-data $N_DATA \
@@ -32,13 +52,13 @@ prio verify1 \
     --shared-secret $SHARED_SECRET \
     --public-key-internal $PUBLIC_KEY_INTERNAL \
     --public-key-external $PUBLIC_KEY_EXTERNAL \
-    --input ${BUCKET_INTERNAL}/raw/data.ndjson \
-    --output ${BUCKET_INTERNAL}/intermediate/internal/verify1
+    --input raw/$filename \
+    --output intermediate/internal/verify1
 
-jq -c '.' ${BUCKET_INTERNAL}/intermediate/internal/verify1/data.ndjson
+jq -c '.' intermediate/internal/verify1/$filename
 
 cp \
-    ${BUCKET_INTERNAL}/intermediate/internal/verify1/data.ndjson \
+    ${BUCKET_INTERNAL}/intermediate/internal/verify1/$filename \
     ${BUCKET_EXTERNAL}/intermediate/external/verify1/
 
 ###########################################################
@@ -53,15 +73,15 @@ prio verify2 \
     --shared-secret $SHARED_SECRET \
     --public-key-internal $PUBLIC_KEY_INTERNAL \
     --public-key-external $PUBLIC_KEY_EXTERNAL \
-    --input ${BUCKET_INTERNAL}/raw/data.ndjson \
-    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/verify1/data.ndjson \
-    --input-external ${BUCKET_INTERNAL}/intermediate/external/verify1/data.ndjson \
+    --input ${BUCKET_INTERNAL}/raw/$filename \
+    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/verify1/$filename \
+    --input-external ${BUCKET_INTERNAL}/intermediate/external/verify1/$filename \
     --output ${BUCKET_INTERNAL}/intermediate/internal/verify2/ \
 
-jq -c '.' ${BUCKET_INTERNAL}/intermediate/internal/verify2/data.ndjson
+jq -c '.' ${BUCKET_INTERNAL}/intermediate/internal/verify2/$filename
 
 cp \
-    ${BUCKET_INTERNAL}/intermediate/internal/verify2/data.ndjson \
+    ${BUCKET_INTERNAL}/intermediate/internal/verify2/$filename \
     ${BUCKET_EXTERNAL}/intermediate/external/verify2/
 
 ###########################################################
@@ -76,15 +96,15 @@ prio aggregate \
     --shared-secret $SHARED_SECRET \
     --public-key-internal $PUBLIC_KEY_INTERNAL \
     --public-key-external $PUBLIC_KEY_EXTERNAL \
-    --input ${BUCKET_INTERNAL}/raw/data.ndjson \
-    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/verify2/data.ndjson \
-    --input-external ${BUCKET_INTERNAL}/intermediate/external/verify2/data.ndjson \
+    --input ${BUCKET_INTERNAL}/raw/$filename \
+    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/verify2/$filename \
+    --input-external ${BUCKET_INTERNAL}/intermediate/external/verify2/$filename \
     --output ${BUCKET_INTERNAL}/intermediate/internal/aggregate/
 
-jq -c '.' ${BUCKET_INTERNAL}/intermediate/internal/aggregate/data.ndjson
+jq -c '.' ${BUCKET_INTERNAL}/intermediate/internal/aggregate/$filename
 
 cp \
-    ${BUCKET_INTERNAL}/intermediate/internal/aggregate/data.ndjson \
+    ${BUCKET_INTERNAL}/intermediate/internal/aggregate/$filename \
     ${BUCKET_EXTERNAL}/intermediate/external/aggregate/
 
 ###########################################################
@@ -99,8 +119,8 @@ prio publish \
     --shared-secret $SHARED_SECRET \
     --public-key-internal $PUBLIC_KEY_INTERNAL \
     --public-key-external $PUBLIC_KEY_EXTERNAL \
-    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/aggregate/data.ndjson \
-    --input-external ${BUCKET_INTERNAL}/intermediate/external/aggregate/data.ndjson \
+    --input-internal ${BUCKET_INTERNAL}/intermediate/internal/aggregate/$filename \
+    --input-external ${BUCKET_INTERNAL}/intermediate/external/aggregate/$filename \
     --output ${BUCKET_INTERNAL}/processed/
 
-jq -c '.' ${BUCKET_INTERNAL}/processed/data.ndjson
+jq -c '.' ${BUCKET_INTERNAL}/processed/$filename
