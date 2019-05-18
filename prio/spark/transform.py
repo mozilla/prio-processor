62d051580fefbab85e824a2c1f129e7a55741955
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-05-17 21:06:36 -0700

    Use libprio, but can't pickle closures

--- prio/spark/transform.py
+++ prio/spark/transform.py
@@ -1,14 +1,5 @@
 from base64 import b64decode, b64encode
-from ..prio import (
-    PRGSeed,
-    PublicKey,
-    PrivateKey,
-    Config,
-    Server,
-    PacketVerify1,
-    PacketVerify2,
-    TotalShare,
-)
+from ..prio import libprio
 from pyspark.sql import SparkSession
 
 
@@ -30,15 +21,7 @@ def publish(df):
 
 import click
 from ..cli import options
-from ..cli.commands import match_server
-
-
-def import_keys(private_key_hex, public_key_hex_internal, public_key_hex_external):
-    return (
-        PrivateKey().import_hex(private_key_hex, public_key_hex_internal),
-        PublicKey().import_hex(public_key_hex_internal),
-        PublicKey().import_hex(public_key_hex_external),
-    )
+from ..cli.commands import match_server, import_keys
 
 
 @click.command()
@@ -63,20 +46,22 @@ def prio_spark(
 ):
     spark = SparkSession.builder.appName("prio").getOrCreate()
 
-    # TODO: Fix this
-    decoded = b64decode(shared_secret)
-    shared_secret = PRGSeed()
-    shared_secret.instance = decoded
+    def create_config():
+        private_key, public_key_internal, public_key_external = import_keys(
+            private_key_hex, public_key_hex_internal, public_key_hex_external
+        )
+        return libprio.PrioConfig_new(
+            n_data, public_key_internal, public_key_external, batch_id
+        )
 
-    private_key, public_key_internal, public_key_external = import_keys(
-        private_key_hex, public_key_hex_internal, public_key_hex_external
-    )
+    def create_server():
+        config = create_config()
+        return libprio.PrioServer_new(
+            config, match_server(server_id), private_key, b64decode(shared_secret)
+        )
 
-    config = Config(n_data, public_key_internal, public_key_external, batch_id)
-    server = Server(config, match_server(server_id), private_key, shared_secret)
-
-    config_var = spark.sparkContext.broadcast(config)
-    server_var = spark.sparkContext.broadcast(server)
+    config_var = spark.sparkContext.broadcast(create_config)
+    server_var = spark.sparkContext.broadcast(create_server)
 
     import code
 
