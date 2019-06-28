6f36137e2c9ef5a576c367a3b885e45a6b5b4f62
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-06-28 14:28:50 -0700

    Add main transform function for flattening data

--- application/prio_processor/staging.py
+++ application/prio_processor/staging.py
@@ -1,6 +1,11 @@
 import click
+import json
+import math
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row, functions as F
+from pyspark.sql.functions import udf, explode, row_number, lit, col, length
+from pyspark.sql.types import StructType, StructField, ArrayType, StringType, MapType
+from pyspark.sql.window import Window
 
 
 def extract(spark, path, date):
@@ -9,8 +14,54 @@ def extract(spark, path, date):
     return spark.read.text(path)
 
 
+payload_schema = StructType(
+    [
+        StructField("id", StringType(), False),
+        StructField(
+            "prioData",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("encoding", StringType(), False),
+                        StructField("prio", MapType(StringType(), StringType()), False),
+                    ]
+                )
+            ),
+            False,
+        ),
+    ]
+)
+
+
+@udf(payload_schema)
+def extract_payload_udf(ping):
+    data = json.loads(ping)
+    return Row(id=data["id"], prioData=data["payload"]["prioData"])
+
+
+def estimate_num_partitions(df, column="prio", partition_size_mb=250):
+    size_b = df.select(F.sum(length(column)).alias("size")).collect()[0].size
+    return math.ceil(size_b * 1.0 / 10 ** 6 / partition_size_mb)
+
+
 def transform(data):
-    return data
+    df = (
+        data.select(extract_payload_udf("value").alias("value"))
+        .select("value.*")
+        .dropDuplicates(["id"])
+        .withColumn("data", explode("prioData"))
+        # drop the id and assign a new one per encoding type
+        # this id is used as a join-key during the decoding process
+        .select(col("data.encoding").alias("batch_id"), "data.prio")
+        .withColumn(
+            "id", row_number().over(Window.partitionBy("batch_id").orderBy(lit(0)))
+        )
+        # explode the values per server
+        .select("batch_id", "id", explode("prio").alias("server_id", "payload"))
+        # reorder the final columns
+        .select("batch_id", "server_id", "id", "payload")
+    )
+    return df
 
 
 def load(data, output):
--- application/tests/test_staging.py
+++ application/tests/test_staging.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from uuid import uuid4
 from click.testing import CliRunner
 from prio_processor import staging
+from pyspark.sql import Row
 
 
 BASE_DATE = "2019-06-26"
@@ -16,6 +17,15 @@ NUM_PINGS = 2
 
 @pytest.fixture()
 def prio_ping():
+    """
+    ```bash
+    $ jq '.payload.prioData | .[] | .encoding' tests/resources/fx-69.0a1.json \
+      | sort | uniq -c
+
+    3 "content.blocking_blocked_TESTONLY-0"
+    3 "content.blocking_blocked_TESTONLY-1"
+    ```
+    """
     path = Path(__file__).parent / "resources" / "fx-69.0a1.json"
     return json.load(open(path))
 
@@ -48,9 +58,52 @@ def moz_fx_data_stage_data(tmpdir, prio_ping):
     return sink_dir
 
 
-def test_extract(spark, moz_fx_data_stage_data):
-    df = staging.extract(spark, moz_fx_data_stage_data, BASE_DATE)
-    assert df.count() == NUM_HOURS * NUM_PARTS * NUM_PINGS
+@pytest.fixture()
+def extracted(spark, moz_fx_data_stage_data):
+    return staging.extract(spark, moz_fx_data_stage_data, BASE_DATE)
+
+
+def test_extract(extracted):
+    assert extracted.count() == NUM_HOURS * NUM_PARTS * NUM_PINGS
+
+
+def test_estimate_num_partitions(spark):
+    df = spark.createDataFrame([Row(prio="#" * 100)] * 100)
+    # 10kb of data with 1k partitions => 10 partitions
+    num_partitions = staging.estimate_num_partitions(df, partition_size_mb=0.001)
+    assert num_partitions == 10
+
+
+def test_transform(extracted):
+    df = staging.transform(extracted)
+
+    assert df.columns == ["batch_id", "server_id", "id", "payload"]
+
+    # 2 servers, 2 batch-ids, 3 blocks, 8 pings
+    assert df.count() == 2 * 2 * 3 * 8
+
+    # check that there are only two servers
+    server_ids = [x.server_id for x in df.select("server_id").distinct().collect()]
+    assert set(server_ids) == {"a", "b"}
+
+    # check the set of "encoding" or "batch-id" fields in the pings
+    batch_ids = [x.batch_id for x in df.select("batch_id").distinct().collect()]
+    assert set(batch_ids) == {
+        "content.blocking_blocked_TESTONLY-0",
+        "content.blocking_blocked_TESTONLY-1",
+    }
+
+    # check that the cardinality of the new dataset corresponds to the values
+    # from the jq expression in the `prio_ping` fixture
+    unique_ids = (
+        df.where("batch_id = 'content.blocking_blocked_TESTONLY-0'")
+        .where("server_id == 'a'")
+        .select("id")
+        .distinct()
+        .count()
+    )
+    # (3 blocks per ping) * (8 pings across the raw dataset)
+    assert unique_ids == 3 * 8
 
 
 def test_staging_run(moz_fx_data_stage_data, tmpdir):
