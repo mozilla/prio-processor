29e051af09ee89447e061c210e37bb6a84d86f32
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-09-03 11:49:15 -0700

    Bump processor to 1.4.0

--- processor/setup.py
+++ processor/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="prio_processor",
-    version="1.3.0",
+    version="1.4.0",
     description="A processing engine for prio data",
     long_description_content_type="text/markdown",
     author="Anthony Miyaguchi",
