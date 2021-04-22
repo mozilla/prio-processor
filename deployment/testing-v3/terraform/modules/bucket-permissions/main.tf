c344dffdb49464f06d60afebba600e9f62630504
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-04-22 16:04:22 -0700

    Fix permissions during testing

--- deployment/testing-v3/terraform/modules/bucket-permissions/main.tf
+++ deployment/testing-v3/terraform/modules/bucket-permissions/main.tf
@@ -12,6 +12,6 @@ resource "google_storage_bucket_iam_member" "shared-internal" {
 
 resource "google_storage_bucket_iam_member" "shared-external" {
   bucket = var.bucket_shared
-  role   = "roles/storage.objectViewer"
+  role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${var.service_account_external}"
 }
