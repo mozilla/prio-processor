e84c3ac0ed2059b17bf35e101f192186de1c60b3
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-04-22 19:07:04 -0700

    Use single file for module

--- deployment/testing-v4/terraform/modules/bucket-permissions/main.tf
+++ deployment/testing-v4/terraform/modules/bucket-permissions/main.tf
@@ -1,3 +1,23 @@
+variable "bucket_private" {
+  type        = string
+  description = "The private bucket for the current processor"
+}
+
+variable "bucket_shared" {
+  type        = string
+  description = "The shared bucket for both processors"
+}
+
+variable "service_account_internal" {
+  type        = string
+  description = "The service account for the current processor"
+}
+
+variable "service_account_external" {
+  type        = string
+  description = "The service account for the co-processor"
+}
+
 resource "google_storage_bucket_iam_member" "private-internal" {
   bucket = var.bucket_private
   role   = "roles/storage.objectAdmin"
