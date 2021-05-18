540161da32f8368b219c5c3bfd2fa2cd19528978
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-05-18 10:42:28 -0700

    Bump version to 4.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prio_processor",
-    version="4.0.0",
+    version="4.1.0",
     description="A processing engine for prio data",
     long_description_content_type="text/markdown",
     author="Anthony Miyaguchi",
